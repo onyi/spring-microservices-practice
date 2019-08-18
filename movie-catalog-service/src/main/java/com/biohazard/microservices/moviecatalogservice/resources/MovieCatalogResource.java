8a7fb6e6dbc80d3830f46d91ea8739830a2ad2e4
Onyi Cho <onyicho@gmail.com>
2019-08-18 12:16:29 -0700

    Update Movie catalog controller to get data from other service

--- movie-catalog-service/src/main/java/com/biohazard/microservices/moviecatalogservice/resources/MovieCatalogResource.java
+++ movie-catalog-service/src/main/java/com/biohazard/microservices/moviecatalogservice/resources/MovieCatalogResource.java
@@ -1,22 +1,47 @@
 package com.biohazard.microservices.moviecatalogservice.resources;
 
+import com.biohazard.microservices.moviecatalogservice.models.Movie;
+import com.biohazard.microservices.moviecatalogservice.models.Rating;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.biohazard.microservices.moviecatalogservice.models.CatalogItem;
+import org.springframework.web.client.RestTemplate;
 
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/catalog")
 public class MovieCatalogResource {
 
+    String MOVIE_SERVICE_URL = "http://localhost:8081/movies/";
+
     @RequestMapping("/{userId}")
     public List<CatalogItem> getCatalog(@PathVariable("userId") String userId){
+
+        RestTemplate restTemplate = new RestTemplate();
+
         System.out.println("User Id: " + userId);
-        return Collections.singletonList(
-            new CatalogItem("helloworld", "Testing", 5)
+
+        // Hardcode rating data for now
+        List<Rating> ratings = Arrays.asList(
+                new Rating("666", 5),
+                new Rating("1234", 4)
         );
+
+        return ratings.stream().map( rating -> {
+            //For each movie ID, request Movie info from Movie Info service
+            Movie movie = restTemplate.getForObject(
+                    MOVIE_SERVICE_URL + rating.getMovieId(), Movie.class);
+
+            return new CatalogItem(movie.getName(), "Testing", rating.getRating());
+        }).collect(Collectors.toList());
+
+//        return Collections.singletonList(
+//            new CatalogItem("helloworld", "Testing", 5)
+//        );
     }
 }
