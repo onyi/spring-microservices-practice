1dc7d11924fb0762ef10588d3721db0f772a9f5c
Onyi Cho <onyicho@gmail.com>
2019-08-17 22:47:32 -0700

    Create CatalogItem model

--- movie-catalog-service/src/main/java/com/biohazard/microservices/moviecatalogservice/models/CatalogItem.java
+++ movie-catalog-service/src/main/java/com/biohazard/microservices/moviecatalogservice/models/CatalogItem.java
@@ -1,4 +1,13 @@
 package com.biohazard.microservices.moviecatalogservice.models;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter @Setter @NoArgsConstructor
 public class CatalogItem {
+
+    private String name;
+    private String desc;
+    private int rating;
 }
