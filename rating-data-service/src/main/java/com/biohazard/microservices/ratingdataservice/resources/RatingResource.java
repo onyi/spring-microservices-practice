41261a6eab7689c63144032c65db904c19980359
Onyi Cho <onyicho@gmail.com>
2019-08-18 10:18:36 -0700

    Add rating controller with psedo result

---
+++ rating-data-service/src/main/java/com/biohazard/microservices/ratingdataservice/resources/RatingResource.java
@@ -0,0 +1,26 @@
+package com.biohazard.microservices.ratingdataservice.resources;
+
+import java.util.Collections;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.biohazard.microservices.ratingdataservice.models.Rating;
+
+import java.util.List;
+
+@RestController
+@RequestMapping('/rating')
+
+public class RatingResource {
+
+    @RequestMapping("{movieId}")
+    public List<Rating> getRating(@PathVariable("movieId") String movieId){
+        return Collections.singletonList(
+            new Rating("1", 5)
+        )
+    }
+
+
+}
