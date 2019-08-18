9585747ec850d2c33caa1b8cafd6dce91c145a64
Onyi Cho <onyicho@gmail.com>
2019-08-18 13:59:27 -0700

    Update movie catalog service controller to request data from other service as the initial microservices structure test

--- movie-catalog-service/src/main/java/com/biohazard/microservices/moviecatalogservice/MovieCatalogServiceApplication.java
+++ movie-catalog-service/src/main/java/com/biohazard/microservices/moviecatalogservice/MovieCatalogServiceApplication.java
@@ -1,10 +1,19 @@
 package com.biohazard.microservices.moviecatalogservice;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class MovieCatalogServiceApplication {
+
+	@Bean
+	public RestTemplate getRestTemplate(){
+		RestTemplate restTemplate = new RestTemplate();
+		return restTemplate;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(MovieCatalogServiceApplication.class, args);
--- movie-catalog-service/src/main/java/com/biohazard/microservices/moviecatalogservice/resources/MovieCatalogResource.java
+++ movie-catalog-service/src/main/java/com/biohazard/microservices/moviecatalogservice/resources/MovieCatalogResource.java
@@ -2,6 +2,7 @@ package com.biohazard.microservices.moviecatalogservice.resources;
 
 import com.biohazard.microservices.moviecatalogservice.models.Movie;
 import com.biohazard.microservices.moviecatalogservice.models.Rating;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,13 +18,13 @@ import java.util.stream.Collectors;
 @RequestMapping("/catalog")
 public class MovieCatalogResource {
 
+    @Autowired
+    RestTemplate restTemplate;
+
     String MOVIE_SERVICE_URL = "http://localhost:8081/movies/";
 
     @RequestMapping("/{userId}")
     public List<CatalogItem> getCatalog(@PathVariable("userId") String userId){
-
-        RestTemplate restTemplate = new RestTemplate();
-
         System.out.println("User Id: " + userId);
 
         // Hardcode rating data for now
