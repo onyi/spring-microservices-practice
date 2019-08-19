512925e81ee1e14e54625a02072561cdee202df5
Onyi Cho <onyicho@gmail.com>
2019-08-18 23:53:06 -0700

    Change rating service to return rating based on userId

--- rating-data-service/src/main/java/com/biohazard/microservices/ratingdataservice/resources/RatingResource.java
+++ rating-data-service/src/main/java/com/biohazard/microservices/ratingdataservice/resources/RatingResource.java
@@ -1,7 +1,9 @@
 package com.biohazard.microservices.ratingdataservice.resources;
 
+import java.util.Arrays;
 import java.util.Collections;
 
+import com.biohazard.microservices.ratingdataservice.models.UserRating;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,7 +13,7 @@ import com.biohazard.microservices.ratingdataservice.models.Rating;
 import java.util.List;
 
 @RestController
-@RequestMapping("/rating")
+@RequestMapping("/ratings")
 
 public class RatingResource {
 
@@ -21,6 +23,17 @@ public class RatingResource {
 //            new Rating("1", 5)
 //        );
         return new Rating(movieId, 5);
+    }
+
+    @RequestMapping("users/{userId}")
+    public UserRating getUserRating(@PathVariable("userId") String userId){
+        List<Rating> ratings = Arrays.asList(
+                new Rating("1234", 5),
+                new Rating("5678", 4)
+        );
+        UserRating userRating = new UserRating();
+        userRating.setUserRating(ratings);
+        return userRating;
     }
 
 
