f28b6f4794fbaac8d20b6c8b7d4e07c42cb8e1b2
Onyi Cho <onyicho@gmail.com>
2019-08-18 22:48:14 -0700

    Add webflux and add a bean to application for WebClient Builder

--- movie-catalog-service/src/main/java/com/biohazard/microservices/moviecatalogservice/config/AppConfig.java
+++ movie-catalog-service/src/main/java/com/biohazard/microservices/moviecatalogservice/config/AppConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
 public class AppConfig {
@@ -16,5 +17,10 @@ public class AppConfig {
         converter.setObjectMapper(new ObjectMapper());
         restTemplate.getMessageConverters().add(converter);
         return restTemplate;
+    }
+
+    @Bean
+    public WebClient.Builder getWebClientBuilder(){
+        return WebClient.builder();
     }
 }
