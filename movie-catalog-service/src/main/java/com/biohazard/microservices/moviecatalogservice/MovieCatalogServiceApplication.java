7f264068683101d470d8a7370de96682e448a640
Onyi Cho <onyicho@gmail.com>
2019-08-20 00:24:49 -0700

    Update movie catalog to register to eureka

--- movie-catalog-service/src/main/java/com/biohazard/microservices/moviecatalogservice/MovieCatalogServiceApplication.java
+++ movie-catalog-service/src/main/java/com/biohazard/microservices/moviecatalogservice/MovieCatalogServiceApplication.java
@@ -3,10 +3,12 @@ package com.biohazard.microservices.moviecatalogservice;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class MovieCatalogServiceApplication {
 
 	@Bean
