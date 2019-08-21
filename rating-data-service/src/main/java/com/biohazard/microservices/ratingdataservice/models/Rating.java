00d68d5bd0a99d5eed0546a2337a3538c71a9f52
Onyi Cho <onyicho@gmail.com>
2019-08-21 15:02:59 -0700

    Add persistence to Rating model

--- rating-data-service/src/main/java/com/biohazard/microservices/ratingdataservice/models/Rating.java
+++ rating-data-service/src/main/java/com/biohazard/microservices/ratingdataservice/models/Rating.java
@@ -4,12 +4,22 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.persistence.*;
+
 @Setter
 @Getter
 @NoArgsConstructor
+@Entity
 public class Rating {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false)
     private int rating;
+
+    @Column(nullable = false)
     private String movieId;
 
     public Rating(String movieId, int rating) {
