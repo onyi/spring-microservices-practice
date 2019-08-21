7fae98e325a9f3f88b3dc9e6aa4eb3a0fc5d928f
Onyi Cho <onyicho@gmail.com>
2019-08-21 16:52:41 -0700

    Add object mapper configuration

---
+++ rating-data-service/src/main/java/com/biohazard/microservices/ratingdataservice/config/AppConfig.java
@@ -0,0 +1,21 @@
+package com.biohazard.microservices.ratingdataservice.config;
+
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.MapperFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfig {
+
+    @Bean
+    ObjectMapper objectMapper(){
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        objectMapper.configure(DeserializationFeature.FAIL_ON_NULL_FOR_PRIMITIVES, false);
+        objectMapper.configure(MapperFeature.DEFAULT_VIEW_INCLUSION, true);
+        return objectMapper;
+    }
+
+}
