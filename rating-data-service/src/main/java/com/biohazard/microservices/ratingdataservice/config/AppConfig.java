96fef97b3f507da3ffc55e61725997f2467868d8
Onyi Cho <onyicho@gmail.com>
2019-08-26 10:52:02 -0700

    Null property should cause error

--- rating-data-service/src/main/java/com/biohazard/microservices/ratingdataservice/config/AppConfig.java
+++ rating-data-service/src/main/java/com/biohazard/microservices/ratingdataservice/config/AppConfig.java
@@ -13,7 +13,7 @@ public class AppConfig {
     ObjectMapper objectMapper(){
         ObjectMapper objectMapper = new ObjectMapper();
         objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
-        objectMapper.configure(DeserializationFeature.FAIL_ON_NULL_FOR_PRIMITIVES, false);
+//        objectMapper.configure(DeserializationFeature.FAIL_ON_NULL_FOR_PRIMITIVES, false);
         objectMapper.configure(MapperFeature.DEFAULT_VIEW_INCLUSION, true);
         return objectMapper;
     }
