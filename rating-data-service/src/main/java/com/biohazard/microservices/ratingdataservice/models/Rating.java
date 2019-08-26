71fb541c0a519af2f8fc5f831aa731d5643ea1e8
Onyi Cho <onyicho@gmail.com>
2019-08-26 11:02:36 -0700

    Add presistence constraints to Rating model

--- rating-data-service/src/main/java/com/biohazard/microservices/ratingdataservice/models/Rating.java
+++ rating-data-service/src/main/java/com/biohazard/microservices/ratingdataservice/models/Rating.java
@@ -3,8 +3,10 @@ package com.biohazard.microservices.ratingdataservice.models;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.validator.constraints.Range;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotNull;
 
 @Setter
 @Getter
@@ -16,13 +18,16 @@ public class Rating {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
-    @Column(nullable = false)
-    private int rating;
+    @Column
+    @NotNull(message = "Rating number cannot be blank!")
+    @Range(min = 1, max = 5, message = "Rating should be from 1 to 5.")
+    private Integer rating;
 
-    @Column(nullable = false)
-    private String movieId;
+    @Column
+    @NotNull(message = "Movie ID is required!")
+    private Integer movieId;
 
-    public Rating(String movieId, int rating) {
+    public Rating(Integer movieId, Integer rating) {
         this.movieId = movieId;
         this.rating = rating;
     }
